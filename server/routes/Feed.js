750e4dcf8841edc82c805786b630823d2fec29f1
Ajinkya-Vijay <ajinkya41198@gmail.com>
2024-07-20 02:00:34 +0530

    Creation of REST APIs

--- server/routes/Feed.js
+++ server/routes/Feed.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {Posts} = require('../models')
 
-router.get("/", (req,res)=>{
- res.send("listenig to feed req");
+router.get("/", async (req,res)=>{
+    const listOfPosts = await Posts.findAll()
+    res.json(listOfPosts);
 });
+
+router.post("/", async (req, res)=>{
+   const post = req.body;
+   await Posts.create(post);
+   res.json(post)
+})
 
 module.exports = router;
 
