5483b20ed6010568ac6a49a0590d50b74fa77f52
Ajinkya-Vijay <ajinkya41198@gmail.com>
2024-07-21 01:41:07 +0530

    Frontend folder struc & landing page

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const app = express();
 
 app.use(express.json())
 const db = require('./models');
+const cors = require('cors')
+
+
+app.use(cors())
 
 const feedRouter = require("./routes/Feed")
 app.use("/feed",feedRouter)
-
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=>{
         console.log("Server is running on 3001")
