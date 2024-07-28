28b7271f5fe7966e48af289f60339c0808a4a8d2
Ajinkya-Vijay <ajinkya41198@gmail.com>
2024-07-28 12:17:55 +0530

    deleting comments

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Post() {
   let { id } = useParams();
   const [postObject, setPostObject] = useState({});
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
+  const { authState } = useContext(AuthContext);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/feed/byId/${id}`).then((response) => {
@@ -21,7 +23,7 @@ function Post() {
   const addComment = () => {
     axios
       .post(
-        "http://localhost:3001/comments",
+        `http://localhost:3001/comments/${id}`,
         {
           commentBody: newComment,
           PostId: id,
@@ -33,17 +35,40 @@ function Post() {
         }
       )
       .then((response) => {
-        if(response.data.error){
-          return  alert(response.data.error)
+        if (response.data.error) {
+          return alert(response.data.error);
         }
-        const commentToAdd = { commentBody: newComment, username : response.data.username };
+        const commentToAdd = {
+          commentBody: newComment,
+          username: response.data.username,
+        };
         setComments([...comments, commentToAdd]);
         setNewComment("");
       });
   };
+
+  const deleteComment = (id) => {
+    console.log(comments.id, id);
+    axios
+      .delete(`http://localhost:3001/comments/${id}`, {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then(() => {
+        setComments(
+          comments.filter((val) => {
+            return val.id !== id;
+          })
+        );
+      })
+      .catch((err) => console.error(err));
+  };
+
   useEffect(() => {
     console.log("comments", comments);
   }, [comments]);
+
   return (
     <div className="postPage">
       <div className="leftSide">
@@ -71,7 +96,18 @@ function Post() {
             return (
               <div key={key} className="comment">
                 {comment.commentBody}
+                {console.log("hotoy na render?")}
                 <label> Username: {comment.username}</label>
+                {authState.username === comment.username ? (
+                  <button
+                    onClick={() => {
+                      console.log("inside");
+                      deleteComment(comment.id);
+                    }}
+                  >
+                    X
+                  </button>
+                ) : null}
               </div>
             );
           })}
--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
+const { where } = require("sequelize");
 
 router.get("/:postId", async (req, res) => {
   const postId = req.params.postId;
@@ -9,12 +10,26 @@ router.get("/:postId", async (req, res) => {
   res.json(comments);
 });
 
-router.post("/", validateToken, async (req, res) => {
+router.post("/:postId", validateToken, async (req, res) => {
+  const postId = req.params.postId;
   const comment = req.body;
-  const username = req.user.username
-  comment.username = username
+  const username = req.user.username;
+  comment.username = username;
   await Comments.create(comment);
-  res.json(comment);
+  const allComments = await Comments.findAll({ where: { PostId: postId } });
+  res.json(allComments);
+});
+
+router.delete("/:commentId", validateToken, async (req, res) => {
+  const commentId = req.params.commentId;
+
+  const id = await Comments.findOne({ where: { id: commentId } });
+  if (!id) return res.json("id not present");
+  await Comments.destroy({
+    where: { id: commentId },
+  });
+
+  res.json("Deleted Succesfully");
 });
 
 module.exports = router;
