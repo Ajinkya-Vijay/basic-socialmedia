11f32ef699a4b057c97286ba1555069ba0db7f67
Ajinkya-Vijay <ajinkya41198@gmail.com>
2024-07-29 19:59:54 +0530

    Liking System for posts

--- server/index.js
+++ server/index.js
@@ -11,10 +11,12 @@ app.use(cors())
 const feedRouter = require("./routes/Feed")
 const commentRouter = require("./routes/Comments")
 const userRouter = require("./routes/Users")
+const likesRouter = require("./routes/Likes")
 
 app.use("/feed",feedRouter)
 app.use("/comments",commentRouter)
 app.use("/auth",userRouter)
+app.use("/likes",likesRouter)
 
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=>{
